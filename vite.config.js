9c027951083331796759d9109f4f54c4d0c9fdba
Nashmi Shah <nashmi.shah@seamlessx.com>
2024-03-03 13:24:53 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/nashmi-personal/",
+  base: '/nashmi-personal/',
   plugins: [react()]
 })
